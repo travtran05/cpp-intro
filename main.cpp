6880b00e5ece27ff7fcd6dc15078a8aa9b92b26a
travistran <travistran544@gmail.com>
2023-07-18 15:45:59 -0400

    problem 6, told to skip 5

--- main.cpp
+++ main.cpp
@@ -97,6 +97,30 @@ class Line{
 };
 
 
+
+// problem 6
+
+class Triangle
+{
+  public:
+    Point p1;
+    Point p2;
+    Point p3;
+
+    double area()
+    {
+      double a = sqrt(pow((p1.x-p2.x),2) + pow((p1.y-p2.y),2));
+      double b = sqrt(pow((p1.x-p3.x),2) + pow((p1.y-p3.y),2));
+      double c = sqrt(pow((p2.x-p3.x),2) + pow((p2.y-p3.y),2));
+      double semi_perim = (a+b+c)/2;
+
+      double area = sqrt(semi_perim*(semi_perim-a)*(semi_perim-b)*(semi_perim-c));
+      return area;
+    }
+};
+
+
+
 int main()
 {
   std::cout << "My name is Travis, my classroom is 115." << std::endl;
