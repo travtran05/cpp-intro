f76586ced461f18deaba3ec3aac779e3212227ad
travistran <travistran544@gmail.com>
2023-07-18 11:48:49 -0400

    started c++

--- main.cpp
+++ main.cpp
@@ -2,11 +2,16 @@
 
 int main()
 {
-  std::cout << "Hello, world!" << std::endl;
+  std::cout << "My name is Travis, my classroom is 115." << std::endl;
 
   int x = 5;
   int y = 7;
-  std::cout << "The sum of " << x << " and " << y << " is " << x + y << std::endl;
+  int z = 2;
+  int w = 1;
+  int v = 3;
+  std::cout << "The sum of " << x << " and " << y << " and " << z << " is " << x + y + z << std::endl;
+  std::cout << "The sum of " << x << " and " << y << " and " << z << " and " << w << " is " << x + y + z + w << std::endl;
+  std::cout << "The division of " << x << " and " << y << " and " << v << " is " << x/y/v << std::endl; 
 
   return 0;
 }
