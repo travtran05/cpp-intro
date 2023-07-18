3286f8454f33bc005cdd032d0e400eb6897c4836
travistran <travistran544@gmail.com>
2023-07-18 14:21:53 -0400

    c++ functions

--- main.cpp
+++ main.cpp
@@ -1,5 +1,24 @@
 #include <iostream>
 
+int subtract(int x, int y) {
+    return x-y;
+}
+
+int multiply(int x, int y) {
+    return x*y;
+
+}
+
+int divide(int x, int y) {
+    if (y!=0) {
+      return x/y;
+    } else {
+      std::cout << "No dividing by 0."; 
+    }
+
+}
+
+
 int main()
 {
   std::cout << "My name is Travis, my classroom is 115." << std::endl;
