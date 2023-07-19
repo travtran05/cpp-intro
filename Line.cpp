5bf67bf9d916e297a48c71a199263bad1a97b1ce
travistran <travistran544@gmail.com>
2023-07-19 10:38:04 -0400

    line done

--- Line.cpp
+++ Line.cpp
@@ -1,21 +1,21 @@
 #include "Line.hpp"
 #include <cmath>
 
-Line::Line(Point p1, Point p2) {
-    this->p1 = p1;
-    this->p2 = p2;
+Line::Line(Point pa, Point pb): _p1(pa), _p2(pb)
+{
+
 }
 
 double Line::length() {
-    return p1.distance_to_point(p2);
+    return this->_p1.distance_to_point(this->_p2);
 }
 
 double Line::distance_to_point(Point p) {
     double x1 = p.x();
     double y1 = p.y();
-    double a = p1.y()-p2.y();
-    double b = -1*(p1.x()-p2.x());
-    double c = p1.x()*p2.y()-p2.x()*p1.y();
+    double a = _p1.y()-_p2.y();
+    double b = -1*(_p1.x()-_p2.x());
+    double c = _p1.x()*_p2.y()-_p2.x()*_p1.y();
 
     return (abs(a*x1+b*y1+c))/(sqrt(pow(a,2)+pow(b,2)));
 }
--- Line.hpp
+++ Line.hpp
@@ -3,25 +3,24 @@
 
 class Line {
     private:
-        Point p1;
-        Point p2;
+        Point _p1;
+        Point _p2;
 
     public:
         Line(Point p1, Point p2);
 
-        Point p1() {return p1;}
-        Point p2() {return p2;}
+        Point p1() {return _p1;}
+        Point p2() {return _p2;}
 
         Point setP1(Point newP1) {
-            p1 = newP1;
+            _p1 = newP1;
         }
 
         Point setP2(Point newP2) {
-            p2 = newP2;
+            _p2 = newP2;
         }
 
         double length();
         double distance_to_point(Point p);
-
 
 };
