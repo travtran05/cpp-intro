aecc19fb9e94b793319726866a86b35cbeba92aa
travistran <travistran544@gmail.com>
2023-07-19 10:10:19 -0400

    10

---
+++ Triangle.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#include "Triangle.hpp"
+#include "Point.hpp"
+
+
+class Triangle {
+    private:
+        Point p1;
+        Point p2;
+        Point p3;
+
+    public:
+        Triangle(Point p1, Point p2, Point p3);
+
+        Point p1() {return p1;}
+        Point p2() {return p2;}
+        Point p3() {return p3;}
+
+        double area();
+
+}
