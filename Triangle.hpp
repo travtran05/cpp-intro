81a2a26f720b37d22d6b8c0c98713b5cebf7750a
travistran <travistran544@gmail.com>
2023-07-19 10:19:34 -0400

    10

--- Triangle.hpp
+++ Triangle.hpp
@@ -17,4 +17,20 @@ class Triangle {
 
         double area();
 
+        Point p1() {return p1;}
+        Point p2() {return p2;}
+        Point p3() {return p3;}
+
+        Point setP1(Point newP1) {
+            p1 = newP1;
+        }
+
+        Point setP2(Point newP2) {
+            p2 = newP2;
+        }
+
+        Point setP3(Point newP3) {
+            p3 = newP3;
+        }
+
 };
